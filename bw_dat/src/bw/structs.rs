7866366f61a8a45581f57cf9aff359980d7fb7b7
PpoPpojaeng-i <quentin0cube1111@gmail.com>
2025-04-10 21:01:40 +0900

    Update structs.rs

--- bw_dat/src/bw/structs.rs
+++ bw_dat/src/bw/structs.rs
@@ -652,7 +652,7 @@ pub struct Player {
     pub storm_id: u32,
     pub player_type: u8,
     pub race: u8,
-    pub race_name: str,
+    pub race_name: *const u8,
     pub race_id: u32,
     pub team: u8,
     pub name: [u8; 25],
